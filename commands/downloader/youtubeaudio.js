462e49eb6783a274862c0f683c49cdfd82809c16
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-01-08 22:10:20 +0700

    [Update] Fix yt downloader mp4/mp3 and remove unused api

--- commands/downloader/youtubeaudio.js
+++ commands/downloader/youtubeaudio.js
@@ -1,49 +1,47 @@
 const {
-  quote
+    quote
 } = require("@mengkodingan/ckptw");
 const axios = require("axios");
 const mime = require("mime-types");
 
 module.exports = {
-  name: "youtubeaudio",
-  aliases: ["yta", "ytaudio", "ytmp3"],
-  category: "downloader",
-  handler: {
-      coin: [10, "text", 1]
-  },
-  code: async (ctx) => {
-      if (await handler(ctx, module.exports.handler)) return;
+    name: "youtubeaudio",
+    aliases: ["yta", "ytaudio", "ytmp3"],
+    category: "downloader",
+    handler: {
+        coin: [10, "text", 1]
+    },
+    code: async (ctx) => {
+        if (await handler(ctx, module.exports.handler)) return;
 
-      const url = ctx.args[0] || null;
+        const url = ctx.args[0] || null;
 
-      if (!url) return await ctx.reply(
-          `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
-          quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/"))
-      );
+        if (!url) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/"))
+        );
 
-      const isUrl = await tools.general.isUrl(url);
-      if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
+        const isUrl = await tools.general.isUrl(url);
+        if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
 
-      try {
-        const apiUrl = tools.api.createUrl("agatz", "/api/ytmp3", {
-              url
-          });
+        try {
+            const apiUrl = tools.api.createUrl("https://ytdl.axeel.my.id", "/api/download/audio", {
+                url
+            });
+            const {
+                data
+            } = await axios.get(apiUrl);
 
-          const {
-            data
-        } = (await axios.get(apiUrl)).data;
-        const result = data.reduce((prev, current) => parseInt(current.quality) > parseInt(prev.quality) ? current : prev);
-
-          return await ctx.reply({
-              audio: {
-                  url: result.downloadUrl
-              },
-              mimetype: mime.lookup("mp3")
-          });
-      } catch (error) {
-          console.error(`[${config.pkg.name}] Error:`, error);
-          if (error.status !== 200) return await ctx.reply(config.msg.notFound);
-          return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
-      }
-  }
+            return await ctx.reply({
+                audio: {
+                    url: data.downloads.url
+                },
+                mimetype: mime.lookup("mp3")
+            });
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
 };
