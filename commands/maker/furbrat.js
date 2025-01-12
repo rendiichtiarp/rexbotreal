4007bbe931d82c43b64f9db403ae79cd2b0b6445
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-01-12 22:55:05 +0700

    [Update] Bump version to 2.29.7 in package.json. Refactor AI chat and downloader commands to use updated API endpoints, improving consistency and response handling. Remove deprecated aliases in the Islamic command and enhance media handling in Instagram and Threads downloaders for better performance.

---
+++ commands/maker/furbrat.js
@@ -0,0 +1,49 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+const {
+    Sticker,
+    StickerTypes
+} = require("wa-sticker-formatter");
+
+module.exports = {
+    name: "furbrat",
+    aliases: ["sfurbrat", "stikerfurbrat", "stickerfurbrat"],
+    category: "maker",
+    handler: {
+        coin: [10, "text", 1]
+    },
+    code: async (ctx) => {
+        if (await handler(ctx, module.exports.handler)) return;
+
+        const input = ctx.args.join(" ") || null;
+
+        if (!input) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "get in the fucking robot, shinji!"))
+        );
+
+        if (input.length > 10000) return await ctx.reply(quote(`❎ Maksimal 50 kata!`));
+
+        try {
+            const apiUrl = tools.api.createUrl("fasturl", "/tool/furbrat", {
+                text: input
+            });
+
+            const sticker = new Sticker(apiUrl, {
+                pack: config.sticker.packname,
+                author: config.sticker.author,
+                type: StickerTypes.FULL,
+                categories: ["🌕"],
+                id: ctx.id,
+                quality: 50
+            });
+
+            return await ctx.reply(await sticker.toMessage());
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
+};
