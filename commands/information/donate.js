2ceb2f2f65663c13e1cfc6f2ccdaa34a9d2391ff
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-01-19 17:22:47 +0700

    [Update] Update various commands to use custom JID and improved reply formatting.

--- commands/information/donate.js
+++ commands/information/donate.js
@@ -11,11 +11,34 @@ module.exports = {
     code: async (ctx) => {
         if (await handler(ctx, module.exports.handler)) return;
 
-        return await ctx.reply(
-            `${monospace("💸 Donasi melalui pembayaran dibawah ini:")}\n\n` +
+         try {
+            const senderJid = ctx.sender.jid;
+            const senderId = senderJid.split(/[:@]/)[0];
+            const customText = await db.get(`bot.text.donate`) || null;
+            const text = customText ?
+                customText
+                .replace(/%tag%/g, `@${senderId}`)
+                .replace(/%name%/g, config.bot.name)
+                .replace(/%version%/g, config.pkg.version)
+                .replace(/%prefix%/g, ctx._used.prefix)
+                .replace(/%command%/g, ctx._used.command)
+                .replace(/%watermark%/g, config.msg.watermark)
+                .replace(/%watermark%/g, config.msg.watermark)
+                .replace(/%footer%/g, config.msg.footer)
+                .replace(/%readmore%/g, config.msg.readmore) :
+                `${monospace("💸 Donasi melalui pembayaran dibawah ini:")}\n\n` +
             `${monospace("📱 QRIS:\nhttps://i.imgur.com/FelT7zk.png")}\n\n` +
             `${monospace("💳 DANA/GOPAY:\n081284900651")}\n\n` +
             `${monospace("Setelah melakukan pembayaran harap hubungi owner dengan mengirimkan bukti pembayaran.")}\n`
-        );
+                "\n" +
+                config.msg.footer;
+            return await ctx.reply({
+                text: text,
+                mentions: [senderJid]
+            });
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
     }
 };
