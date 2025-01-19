10e6355b34f9525d3bcd97c7cee530f20e3ef7d8
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-01-19 18:25:13 +0700

    [Update] Add feature Brat Animation

---
+++ commands/maker/bratanimate.js
@@ -0,0 +1,48 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+const {
+    Sticker,
+    StickerTypes
+} = require("wa-sticker-formatter");
+
+module.exports = {
+    name: "abrat",
+    aliases: ["asbrat", "stikerabrat", "stickerabrat"],
+    category: "maker",
+    handler: {
+        limit: 1,
+        coin: 10
+    },
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || null;
+
+        if (!input) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx._used, "get in the fucking robot, shinji!"))
+        );
+
+        if (input.length > 10000) return await ctx.reply(quote(`❎ Maksimal 50 kata!`));
+
+        try {
+            const apiUrl = tools.api.createUrl("https://brat.caliphdev.com", "/api/brat/animate", {
+                text: input
+            });
+
+            const sticker = new Sticker(apiUrl, {
+                pack: config.sticker.packname,
+                author: config.sticker.author,
+                type: StickerTypes.FULL,
+                categories: ["🌕"],
+                id: ctx.id,
+                quality: 50
+            });
+
+            return await ctx.reply(await sticker.toMessage());
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
+};
