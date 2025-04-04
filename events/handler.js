8a4d3e4e79fd453c0cdc3b8eb2e4f3baab52af17
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-04-04 21:51:15 +0700

    refactor: perbaiki format ID pengguna dalam penanganan acara
    - Memperbaiki format ID pengguna dengan memindahkan simbol '@' ke depan ID untuk meningkatkan kejelasan dalam pesan yang dikirim saat pengguna bergabung atau keluar dari grup.

--- events/handler.js
+++ events/handler.js
@@ -36,8 +36,8 @@ async function handleUserEvent(bot, m, type) {
                     const profilePictureUrl = await bot.core.profilePictureUrl(jid, "image").catch(() => "https://i.pinimg.com/736x/70/dd/61/70dd612c65034b88ebf474a52ccc70c4.jpg");
         
                     const customText = type === "UserJoin" ? groupDb?.welcome_text : groupDb?.goodbye_text;
-                    const userId = `@${tools.general.getID(jid)}`;
-                    const userTag = `${userId}`;
+                    const userId = tools.general.getID(jid);
+                    const userTag = `@${userId}`;
         
                     const text = customText ?
                         customText
