1e8ed823e9571ae77fa4ce6aa26f7e33cb1a06d8
rendiichtiarp <rendiichtiarprasetyo@gmail.com>
2025-04-09 15:16:23 +0700

    perbarui: perbaiki logika pengambilan input dan perbarui beberapa perintah
    - Memperbaiki logika pengambilan input pada beberapa perintah untuk meningkatkan konsistensi dan keandalan.
    - Mengganti beberapa referensi pengambilan teks dari objek yang dikutip untuk menggunakan metode yang lebih efisien.
    - Memperbarui daftar kontributor dalam perintah informasi untuk mencerminkan perubahan terbaru.

--- commands/owner/broadcastgc.js
+++ commands/owner/broadcastgc.js
@@ -10,7 +10,7 @@ module.exports = {
         owner: true
     },
     code: async (ctx) => {
-        const input = ctx.args.join(" ") || (q => q?.conversation || q?.text || q?.caption)(Object.values(ctx.quoted || {}).find(v => typeof v === "object")) || null;
+        const input = ctx.args.join(" ") || ctx.quoted?.conversation || Object.values(ctx.quoted).map(v => v?.text || v?.caption).find(Boolean); || null;
         const msgType = ctx.getMessageType();
         const [checkMedia, checkQuotedMedia] = await Promise.all([
             tools.cmd.checkMedia(msgType, "image"),
